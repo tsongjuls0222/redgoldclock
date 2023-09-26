8224ba85496304d4c3581687daaa8923fc9f62c4
WILLIAMSHAKESIT <williamonacle@gmail.com>
2023-09-26 10:53:45 +0800

    fgjhndfkgujbdfkgjb

--- js/layout.js
+++ js/layout.js
@@ -1,33 +1,40 @@
-// setInterval(() => {
-//     d = new Date(); //object of date()
-//     hr = d.getHours();
-//     min = d.getMinutes();
-//     sec = d.getSeconds();
-//     hr_rotation = 30 * hr + min / 2; //converting current time
-//     // min_rotation = 6 * min;
-//     // sec_rotation = 6 * sec;
- 
-//     hour.style.transform = `rotate(${hr_rotation}deg)`;
-//     minute.style.transform = `rotate(${min_rotation}deg)`;
-//     second.style.transform = `rotate(${sec_rotation}deg)`;
-// }, 1000);
+
+const fullscreenButton = document.getElementById('fullscreen');
+let fsBool = false;
+let fullScreen = document.documentElement;
+
+fullscreenButton.addEventListener('click', toggleFullScreen);
+
+function toggleFullScreen(){
+    if(!fsBool){
+        fsBool = true;
+        openFullscreen();
+    }else{
+        fsBool = false;
+        closeFullscreen();
+    }
+}
+
+function openFullscreen(){
+    if (fullScreen.requestFullscreen) {
+        fullScreen.requestFullscreen();
+    } else if (fullScreen.webkitRequestFullscreen) { /* Safari */
+        fullScreen.webkitRequestFullscreen();
+    } else if (fullScreen.msRequestFullscreen) { /* IE11 */
+        fullScreen.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
 
 $(document).ready(function(){
-    let time = 10
-    let el = $('.gold.animate__animated')
-    setInterval(function(){
-        time--
-        // console.log(time)
-        $('.digit').text(`${time}`)
-        // if(time == 9){
-        //     el.addClass('animate__flipInX')
-        // }else if(time == 5){
-        //     el.removeClass('animate__flipInX')
-        // }else if(time == 1){
-        //     time = 10
-        // }
-        if(time == 1){
-            time = 10
-        }
-    },1000)
+    // $()
 })
