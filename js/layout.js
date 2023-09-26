7b14235f1ca3ced8f2aae490ce97bd411e527bae
WILLIAMSHAKESIT <williamonacle@gmail.com>
2023-09-26 10:21:18 +0800

    dfjhnlkfjbsf

--- js/layout.js
+++ js/layout.js
@@ -1,28 +1,32 @@
-setInterval(() => {
-    d = new Date(); //object of date()
-    hr = d.getHours();
-    min = d.getMinutes();
-    sec = d.getSeconds();
-    hr_rotation = 30 * hr + min / 2; //converting current time
-    min_rotation = 6 * min;
-    sec_rotation = 6 * sec;
+// setInterval(() => {
+//     d = new Date(); //object of date()
+//     hr = d.getHours();
+//     min = d.getMinutes();
+//     sec = d.getSeconds();
+//     hr_rotation = 30 * hr + min / 2; //converting current time
+//     // min_rotation = 6 * min;
+//     // sec_rotation = 6 * sec;
  
-    hour.style.transform = `rotate(${hr_rotation}deg)`;
-    minute.style.transform = `rotate(${min_rotation}deg)`;
-    second.style.transform = `rotate(${sec_rotation}deg)`;
-}, 1000);
+//     hour.style.transform = `rotate(${hr_rotation}deg)`;
+//     minute.style.transform = `rotate(${min_rotation}deg)`;
+//     second.style.transform = `rotate(${sec_rotation}deg)`;
+// }, 1000);
 
 $(document).ready(function(){
     let time = 10
     let el = $('.gold.animate__animated')
     setInterval(function(){
         time--
-        console.log(time)
-        if(time == 9){
-            el.addClass('animate__flipInX')
-        }else if(time == 5){
-            el.removeClass('animate__flipInX')
-        }else if(time == 1){
+        // console.log(time)
+        $('.digit').text(`${time}`)
+        // if(time == 9){
+        //     el.addClass('animate__flipInX')
+        // }else if(time == 5){
+        //     el.removeClass('animate__flipInX')
+        // }else if(time == 1){
+        //     time = 10
+        // }
+        if(time == 1){
             time = 10
         }
     },1000)
